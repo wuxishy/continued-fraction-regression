02ae8df90eede75beefabb3c348addc5ee1686e6
Haoyuan Sun <wuxidavid@hotmail.com>
2018-07-30 10:09:57 +1000

    more efficient IO

--- data.cpp
+++ data.cpp
@@ -8,7 +8,6 @@
 #include "fraction.hpp"
 
 #include <fstream>
-#include <sstream>
 #include <string>
 
 #include <cmath>
@@ -19,22 +18,18 @@ void data_store::read(const char* filename) {
     std::ifstream fin(filename);
    
     fin >> num_entry >> num_var;
-    fin.ignore();
 
     std::string line;
     for(int i = 0; i < num_entry; ++i) {
-        std::getline(fin, line);
-        
         double tmp;
-        std::istringstream tokenizer(line);
 
-        tokenizer >> tmp;
+        fin >> tmp;
         expected.push_back(tmp);
 
         input.push_back(vector<double>());
         
         for (int j = 0; j < num_var; ++j) {
-            tokenizer >> tmp;
+            fin >> tmp;
             input.back().push_back(tmp);
         }
 
@@ -59,7 +54,7 @@ void data_store::print_val(const char* filename, fraction& frac) {
     std::ofstream fout(filename);
     
     for(int i = 0; i < num_entry; ++i)
-        fout << expected[i] << ',' << frac.eval(input[i]) << std::endl;
+        fout << expected[i] << ',' << frac.eval(input[i]) << '\n';
 
     fout.close();
 }
